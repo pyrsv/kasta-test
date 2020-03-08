81d6aa5ad1db34bbfd12e2c9f9cd27842ffa7fde
Nikita Pyresev <wegofender@gmail.com>
2020-03-08 22:08:02 +0200

    Updates

---
+++ precache-manifest.a3ca542001a50aac1542d245f432caef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299735c5699c4e12a2067b4c20481147",
+    "url": "/kasta-test/index.html"
+  },
+  {
+    "revision": "64ba40a76cf7f11e739e",
+    "url": "/kasta-test/static/css/main.d4d2c9d9.chunk.css"
+  },
+  {
+    "revision": "15652ec5e7ef00cde92e",
+    "url": "/kasta-test/static/js/2.8a783dfa.chunk.js"
+  },
+  {
+    "revision": "076350a21fcbc96beacccfb381d0deff",
+    "url": "/kasta-test/static/js/2.8a783dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ba40a76cf7f11e739e",
+    "url": "/kasta-test/static/js/main.17451c0e.chunk.js"
+  },
+  {
+    "revision": "7d93a799caf405ff4345",
+    "url": "/kasta-test/static/js/runtime-main.e7644efe.js"
+  },
+  {
+    "revision": "063ecc7489c7690cc9364cb8b5c5a39d",
+    "url": "/kasta-test/static/media/logo.063ecc74.svg"
+  }
+]);
